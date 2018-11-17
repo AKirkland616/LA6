0c380b58e96ba0eba877a66724cea1c5bfbd8416
anthonykirkland <anthonykirkland@wireless140-020.wireless.wmich.edu>
2018-11-16 19:54:10 -0500

    Anthony Commit 2 some initial thoughts written in code. Encoder and Decoder class.

--- LA6/src/LA6/edu/wmich/cs1120/Decoder.java
+++ LA6/src/LA6/edu/wmich/cs1120/Decoder.java
@@ -1,11 +1,50 @@
 package LA6.edu.wmich.cs1120;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Decoder implements IDecoder {
 
 	@Override
 	public void decode(String filePath) {
 		// TODO Auto-generated method stub
 
+		File f = new File(filePath);
+		try {
+			Scanner reader = new Scanner(f);
+			ArrayList<Character> a = new ArrayList<>();
+			//reader.
+			while(reader.hasNextLine()) {
+				char[] idk = reader.nextLine().toCharArray();
+				for (int i=0;i<idk.length;i++) {
+					
+				if(idk[i]=='z') {
+					break;
+				}
+				if (Character.isDigit(idk[i]) && Character.getNumericValue(idk[i]) != -1) {
+					int skip=Character.getNumericValue(idk[i]);
+					i = i+(skip-1);
+				}
+				else {
+					a.add(idk[i]);
+				//	System.out.print(idk[i]);
+				}
+				}
+			}
+			for(int i =0;i<a.size();i++) {
+				if (a.get(i)=='-') {
+					System.out.print("\n");
+				}else {
+					System.out.print(a.get(i));
+				}
+				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
 		
 	}
 
--- LA6/src/LA6/edu/wmich/cs1120/Encoder.java
+++ LA6/src/LA6/edu/wmich/cs1120/Encoder.java
@@ -1,11 +1,59 @@
 package LA6.edu.wmich.cs1120;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Encoder implements IEncoder {
 
 	@Override
 	public void encode(String inputFileName, String outputFilePath) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub				
+		File f = new File(inputFileName);
+		try {
+		Scanner reader = new Scanner(f);
+		ArrayList<Character> a = new ArrayList<>();
+		while(reader.hasNextLine()) {
+		String s = reader.nextLine();
+		Random rand = new Random();
+		char[] v = s.toCharArray();
+		for (int i =0;i<v.length;i++) {
+			a.add(v[i]);
+			if(i!=s.length()-1) {
+				String p = Integer.toString((rand.nextInt(20)+1));
+				a.add(p.toCharArray()[0]);
+			}
+			
+		}
+		}
+		a.add('z');
+		RandomAccessFile randomFile = new RandomAccessFile(outputFilePath, "rw");
+		for(int i =0;i<a.size();i++) {
+			try {
+				randomFile.writeChar(a.get(i));
+				if(Character.isDigit(a.get(i)) && Character.getNumericValue(a.get(i))!= -1) {
+					int b = Character.getNumericValue(a.get(i));
+					for(int z =1;z<b;z++) {
+						randomFile.write(' ');
+					}
+				}else if (a.get(i)== 'z') {
+					break;
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		//randomFile.w);
+		
+		}catch(FileNotFoundException e) {
+			//e.getMessage();
+		}
+		
 		
 	}
 
